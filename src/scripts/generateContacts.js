38c742641a96378cf0a415d6243463497546f4e5
AliceMozyrko <mozyrkoalisa@gmail.com>
2024-08-26 12:17:30 +0300

    finish

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,5 +1,12 @@
-import { PATH_DB } from '../constants/contacts.js';
+import { updateContacts } from './updateContacts.js';
+import { createFakeContact } from '../utils/createFakeContact.js';
+import { getAllContacts } from "./getAllContacts.js";
 
-const generateContacts = async (number) => {};
+const generateContacts = async (number) => {
+  const contactList = getAllContacts();
+  const newContacts = Array(number).fill(0).map(createFakeContact);
+  contactList.push(...newContacts);
+  await updateContacts(contactList);
+};
 
-generateContacts(5);
+generateContacts(2);
--- src/scripts/removeAllContacts.js
+++ src/scripts/removeAllContacts.js
@@ -1,5 +1,8 @@
-import { PATH_DB } from '../constants/contacts.js';
+import { updateContacts } from "./updateContacts.js";
 
-export const removeAllContacts = async () => {};
+export const removeAllContacts = async () => {
+  await updateContacts([]);
+
+};
 
 removeAllContacts();
